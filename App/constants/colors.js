dda4c669449d6e46ac0d3fb46f960c611f3b54db
Dans Domanevskis <dandomanevski@gmail.com>
2020-12-26 15:11:00 +0200

    Module 3 lesson 1 complete

---
+++ App/constants/colors.js
@@ -0,0 +1,4 @@
+export default {
+    text: '#343434',
+    borderColor: '#E2E2E2',
+};
--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,13 +1,21 @@
 import React from 'react'; 
 import { View, TouchableOpacity, Text, SafeAreaView, StyleSheet } from 'react-native';
 
+import colors from './constants/colors'
+
 const styles = StyleSheet.create({
     row: {
-        paddingHorizontal = 20,
-        paddingVertical = 15,
+        paddingHorizontal: 20,
+        paddingVertical: 15,
     },
     text: {
-
+        fontSize: 16,
+        color: colors.text,
+    },
+    separator: {
+        backgroundColor: colors.backgroundColor,
+        height: StyleSheet.hairlineWidth,
+        marginLeft: 20,
     }
 });
 
@@ -18,9 +26,13 @@ export default () => {
                 <Text style={styles.text}>Themes</Text>
             </TouchableOpacity>
 
+            <View style={styles.separator} />
+
             <TouchableOpacity style={styles.row}>
                 <Text style={styles.text}>React Native Basics</Text>
             </TouchableOpacity>
+
+            <View style={styles.separator} />
 
             <TouchableOpacity style={styles.row}>
                 <Text style={styles.text}>React Native by Example</Text>
