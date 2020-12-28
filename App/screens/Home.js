2206bc655f1ff5598bf90fbaea072d56d0c938cf
Dans Domanevskis <dandomanevski@gmail.com>
2020-12-28 14:46:19 +0200

    Module 4 complete

---
+++ App/screens/Home.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, StatusBar, Image, Dimensions, Text, ScrollView } from 'react-native';
+import { format } from 'date-fns';
+
+import Colors from '../constants/colors';
+import { ConversionInput } from '../components/ConversionInput';
+import colors from '../constants/colors';
+import { Button } from '../components/Button';
+import { KeyboardSpacer } from '../components/KeyboardSpacer';
+
+const screen = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: Colors.blue,
+        flex: 1
+    },
+    content: {
+        paddingTop: screen.height * 0.2,
+    },
+    logoContainer: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    logoBackground: {
+        width: screen.width * 0.45,
+        height: screen.height * 0.45,
+    },
+    logo: {
+        position: 'absolute',
+        width: screen.width * 0.25,
+        height: screen.height * 0.25,
+    },
+    textHeader: {
+        color: colors.white,
+        fontWeight: 'bold',
+        fontSize: 30,
+        marginVertical: 20,
+        textAlign: 'center'
+    },
+    text: {
+        color: colors.white,
+        fontSize: 14,
+        textAlign: 'center'
+    }
+});
+
+export default () => {
+    const baseCurrency = 'EUR';
+    const quoteCurrency = 'USD';
+    const conversionRate = 1.22;
+    const date = new Date();
+
+    const [scrollEnabled, setScrollEnabled] = useState()
+
+    return (
+        <View style={styles.container}>
+            <ScrollView scrollEnabled={scrollEnabled}>
+                <StatusBar barStyle = 'light-content' backgroundColor = {Colors.blue}/>
+                <View style={styles.content}>
+                    <View style={styles.logoContainer}>
+                        <Image source={require('../assets/images/background.png')} style={styles.logoBackground} resizeMode='contain'/>
+                        <Image source={require('../assets/images/logo.png')} style={styles.logo} resizeMode='contain'/>
+                    </View>
+
+                    <Text style={styles.textHeader}>Currency Converter</Text>
+
+                    <ConversionInput 
+                    text = {baseCurrency}
+                    value = '123'
+                    onButtonPress = {() => alert('todo!')}
+                    onChangeText = {text => console.log('text', text)}
+                    keyboardType = 'numeric'/>
+                    
+                    <ConversionInput
+                    text = {quoteCurrency}
+                    value = '123'
+                    onButtonPress = {() => alert('todo!')}
+                    editable = {false}/>
+
+                    <Text style={styles.text}>{`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(date, 'MMMM do yyyy')}.`}</Text>
+
+                    <Button text='Reverse Currencies' onPress={() => alert('todo!')}/>
+
+                    <KeyboardSpacer onToggle={(keyboardIsVisible) => setScrollEnabled(keyboardIsVisible)} />
+
+                </View>
+            </ScrollView>
+        </View>
+    );
+};
